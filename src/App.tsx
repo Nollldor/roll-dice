ce378f37a79e1e9bd4095ddf5ab726f38aed9e3d
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-11-01 18:54:48 +0300

    v2: material ui, sum of dices, button group, styles improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,62 @@
 import React, {useState} from 'react';
 import './App.css';
-import {D20} from "./component/D20";
-import {getRandomInt} from "./common/utils";
+import {D20} from "./component/D20/D20";
+import {getRandomArrayOfInts, sumOfArray} from "./common/utils";
+import {Button, ButtonGroup, Container, Typography} from "@mui/material";
 
 function App() {
-    const [rolledNum, setRolledNum] = useState<number | null>(null)
-
+    const [rolledNums, setRolledNums] = useState<Array<number | null>>([null])
+    const [rollingMode, setRollingMode] = useState(false)
+    const [sum,setSum] = useState(0)
     const rollDice = () => {
+        setRollingMode(true)
         const newInterval = setInterval(() => {
-            setRolledNum(getRandomInt(20))
+            setRolledNums(getRandomArrayOfInts(20, rolledNums.length))
+            setSum(sumOfArray(rolledNums))
         }, 100)
         setTimeout(() => {
             clearInterval(newInterval)
+            setRollingMode(false)
         }, 3000)
     }
 
+    const addDice = () => {
+        setRolledNums([...rolledNums, null])
+        setSum(0)
+    }
+
+    const removeDice = () => {
+        const copyRolledNums = [...rolledNums]
+        copyRolledNums.pop()
+        setRolledNums(copyRolledNums)
+        setSum(0)
+    }
 
     return (
         <div className="App">
-            <D20 value={rolledNum}/>
-            <div>
-                <button onClick={rollDice}>roll</button>
+            <div className={'controlButtons'}>
+                <Container maxWidth='sm'>
+                    <ButtonGroup size='medium' fullWidth={true} variant='contained'
+                                 aria-label="outlined primary button group">
+                        <Button disabled={rolledNums.length < 1 || rollingMode} onClick={rollDice}>roll</Button>
+                        <Button disabled={rollingMode} onClick={addDice}>+dice</Button>
+                        <Button disabled={rolledNums.length < 1 || rollingMode} onClick={removeDice}>-dice</Button>
+                    </ButtonGroup>
+                </Container>
             </div>
+            <div className={'sumBlock'}>
+                <Typography variant={"h6"}>
+                    Sum of dices: {!rollingMode && sum}
+                </Typography>
+            </div>
+
+            <div className={"dicesBlock"}>
+                {rolledNums.map((num, index) => (
+                    <D20 key={index} value={num}/>
+                ))}
+            </div>
+
+
         </div>
     );
 }
--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,3 +1,19 @@
 export function getRandomInt(max: number) {
     return Math.floor(Math.random() * max) + 1;
 }
+
+export const getRandomArrayOfInts = (max: number, lenght: number) => {
+    const resultArr = []
+
+    for (let i = 0; i < lenght; i++) {
+        resultArr.push(getRandomInt(max))
+    }
+
+    return resultArr
+}
+
+export const sumOfArray = (arr: Array<number | null>):number => {
+    return arr.map(el=>el===null? 0 : el).reduce((prev, current) => {
+        return current === null ? prev + 0 : prev + current
+    }, 0)
+}
