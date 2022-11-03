615dd172a29855bf04cd60d32c4e84bdc64c9870
Arthur Ishmiev <artur.ishmiev@gmail.com>
2022-11-03 18:41:30 +0300

    v6: style improvements

--- src/component/SumBlock/SumBlock.tsx
+++ src/component/SumBlock/SumBlock.tsx
@@ -1,17 +1,30 @@
 import React, {FC} from "react";
-import {Typography} from "@mui/material";
+import {Container, createTheme, Paper, ThemeProvider, Typography} from "@mui/material";
 import style from './SumBlock.module.css'
+import {blue} from "@mui/material/colors";
 
 type PropsType = {
     rollingMode: boolean
     sum: number
 }
 
-export const SumBlock:FC<PropsType> = ({rollingMode, sum}) => {
 
-    return <div className={style.sumBlock}>
-        <Typography variant={"h6"}>
-            Sum of dices: {!rollingMode && sum}
-        </Typography>
-    </div>
+export const SumBlock: FC<PropsType> = ({rollingMode, sum}) => {
+
+    return <Container maxWidth='sm' className={style.sumBlock}>
+        <Paper elevation={10} sx={{
+            bgcolor: '#1976d2',
+            color: '#fff',
+            height: "100%",
+        }}>
+            <Typography variant={"h6"}>
+                Sum of dices:
+            </Typography>
+
+            <Typography variant={"h6"}>
+                {!rollingMode && sum}
+            </Typography>
+        </Paper>
+    </Container>
+
 }
